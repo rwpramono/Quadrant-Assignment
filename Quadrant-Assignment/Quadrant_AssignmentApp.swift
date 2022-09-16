c60a0a5b7632c53d82eb45891e06d719227971c4
R Wahyu Pramono <rachmatwahyu@mamiteam.com>
2022-09-16 21:04:27 +0700

    Initial Commit

---
+++ Quadrant-Assignment/Quadrant_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadrant_AssignmentApp.swift
+//  Quadrant-Assignment
+//
+//  Created by Rachmat Wahyu Pramono on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Quadrant_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
